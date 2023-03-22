5c2cc035083dfc72e16547a454a2d2272bae8494
prabhat1001 <prabhatsaraswat24@gmail.com>
2023-03-22 13:42:58 +0530

    initial commit

---
+++ mui/src/App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './App.css';
+import Home from './Pages/Home';
+
+export default function MyApp() {
+  return (
+    <div>
+        <Home />
+    </div>
+  );
+}
